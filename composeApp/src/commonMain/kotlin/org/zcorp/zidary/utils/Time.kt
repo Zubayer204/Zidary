a9cc6cded54647b3c395c40d40c92f1743740583
Zubayer Rahaman <zubayer.skd@gmail.com>
2024-12-27 07:20:50 +0600

    feat: Made time formatting follow 12h style

--- composeApp/src/commonMain/kotlin/org/zcorp/zidary/utils/Time.kt
+++ composeApp/src/commonMain/kotlin/org/zcorp/zidary/utils/Time.kt
@@ -9,11 +9,18 @@ import kotlinx.datetime.plus
 import kotlinx.datetime.toLocalDateTime
 import kotlinx.datetime.until
 
-fun formatDateTime(dateTime: LocalDateTime): String {
+fun formatDateTime(dateTime: LocalDateTime, hourStyle: Int = 12): String {
     val month = dateTime.month.name.take(3).lowercase().replaceFirstChar { it.uppercase() }
     val day = dateTime.dayOfMonth.toString().padStart(2, '0')
     val hour = dateTime.hour.toString().padStart(2, '0')
     val minute = dateTime.minute.toString().padStart(2, '0')
+    val amPm = if (dateTime.hour < 12) "AM" else "PM"
+    val hour12 = if (hourStyle == 12) (dateTime.hour % 12).let { if (it == 0) 12 else it } else hour
+
+    if (hourStyle == 12) {
+        return "$day $month, ${dateTime.year}  $hour12:$minute $amPm"
+    }
+
     return "$day $month, ${dateTime.year}  $hour:$minute"
 }
 
